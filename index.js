56ae14b972a6f3847c88d97f42cfb09c08d0acfc
Nathan Zadoks <nathan@nathan7.eu>
2013-04-30 00:04:06 +0200

    cleaner browserify compat

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = require('./main')(require('json'))
+var xhr = require('xhr')
+  , Json
+try { Json = require('jsonify') } catch(e) {}
+if (!Json) Json = require('json')
+
+module.exports = 
+function xhrJSON(url, opts) {
+  return xhr(url, opts).then(parse, parseErr)
+  function parse(res) {
+    res.body = Json.parse(res.body)
+    return res
+  }
+  function parseErr(res) {
+    if (res.body) try { parse(res) } catch(e) {}
+    throw res
+  }
+}
