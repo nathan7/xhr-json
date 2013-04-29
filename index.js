b710ff705ff69fed0cf9eac8dcc33bfe41994f59
Nathan Zadoks <nathan@nathan7.eu>
2013-04-29 23:15:48 +0200

    2.0.0

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-var xhr = require('xhr')
-  , get = require('get')
-  , json = require('json')
-module.exports =
-function xhrJSON(url, opts) {
-  return xhr(url, opts)
-    .then(get('responseText'))
-    .then(json.parse)
-}
+module.exports = require('./main')(require('json'))
